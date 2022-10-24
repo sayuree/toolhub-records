99900ca831bf533d3b67155666ab79728b7b60d8
sabina.zaripova <sabina@enhancera.com>
2022-10-24 14:17:22 +0600

    navbar: Add responsiveness and side menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/toolhub_community_logo.svg';
 
 const Navbar  = () => {
-    return(
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return (
         <div className='navbar'>
             <div className='navbar-links'>
                 <div className='navbar-links_logo'>
@@ -17,6 +20,24 @@ const Navbar  = () => {
                 <div className='navbar-sign'>
                     <p>Log in</p>
                     <button type='button'>Sign up</button>
+                </div>
+                <div className='navbar-menu'>
+                    {toggleMenu
+                    ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+                    : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>}
+                    {toggleMenu && (
+                        <div className='navbar-menu_container scale-up center'>
+                            <div className='navbar-menu_container-links'>
+                                <p><a href="#home">Home</a></p>
+                                <p><a href="#leaderboard">Leaderboard</a></p>
+                                <p><a href="#dashboard">Dashboard</a></p>
+                            </div>
+                            <div className="navbar-menu_container-links-sign">
+                                <p>Sign in</p>
+                                <button type="button">Sign up</button>
+                             </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
